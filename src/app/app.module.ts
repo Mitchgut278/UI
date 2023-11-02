56a29cfd68af90b5f2368a0ce250bd7685f000e4
Mitchgut278 <gutmanmitchell@gmail.com>
2023-11-02 17:52:28 -0400

    move toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { ToolbarComponent } from './landing-page/toolbar/toolbar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 import { MenubarModule } from 'primeng/menubar';
 import { AboutPageComponent } from './about-page/about-page.component';
