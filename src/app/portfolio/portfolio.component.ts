52e10a0484e379f63a3e7764c88d911a0654c3ed
Mitchgut278 <gutmanmitchell@gmail.com>
2023-12-13 11:45:32 -0500

    nest portfolio images

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 
+  images = [
+    'path/to/image1.png',
+    'path/to/image2.png',
+    // ... more images
+  ];
+
+  currentIndex = 0;
+
+  scrollLeft() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+
+  scrollRight() {
+    if (this.currentIndex < this.images.length - 3) {
+      this.currentIndex++;
+    }
+  }
+
 }
